ad9ecd681cd07687c3edd0934742c2485214611c
Talon Ezio <vuthemanh1707@gmail.com>
2024-04-12 01:25:53 +0700

    Hoàn thiện tool

--- Program.cs
+++ Program.cs
@@ -1,18 +1,26 @@
-﻿using System.Data.SqlClient;
-using System.Diagnostics;
+﻿using System.Diagnostics;
 using System.Text;
-using Dapper;
-using Xabe.FFmpeg;
+using Microsoft.Extensions.Configuration;
 
 namespace FFMPEGEncoder
 {
     internal class Program
     {
+        private static readonly string BasePath = Path.GetDirectoryName(System.Reflection.Assembly.GetEntryAssembly()!.Location)!;
         static async Task Main()
         {
+            var config = new ConfigurationBuilder()
+                .SetBasePath(BasePath)
+                .AddJsonFile("appsettings.json")
+                .Build();
+
+            bool enableCmd = Convert.ToBoolean(config[nameof(enableCmd)]);
+            bool deleteAfterEncode = Convert.ToBoolean(config[nameof(deleteAfterEncode)]);
+
             Console.InputEncoding = Console.OutputEncoding = Encoding.Unicode;
 
             string defaultFontName = "Arial";
+
             Console.Write($"Tên font (mặc định {defaultFontName}): ");
             var fontName = Console.ReadLine();
 
@@ -30,9 +38,17 @@ namespace FFMPEGEncoder
                 fontSize = defaultFontSize;
             }
 
-            string logo = "Images/logo.png";
+            string logo = Path.Combine(BasePath, "Images", "Logo.png");
 
-            const int bitRate = 1500;
+            int bitRate;
+
+            bool checkBitrate;
+            do
+            {
+                Console.Write("Bitrate (1000 <= x <= 5000): ");
+
+                checkBitrate = int.TryParse(Console.ReadLine(), out bitRate);
+            } while (!checkBitrate && bitRate < 1000 || bitRate > 5000);
 
             string path;
             do
@@ -44,19 +60,19 @@ namespace FFMPEGEncoder
             var files = Directory.GetFiles(path, "*.mkv", SearchOption.AllDirectories);
 
             Console.WriteLine($"Chuẩn bị xử lý {files.Length} files...");
-
-            FFmpeg.SetExecutablesPath("Tools");
-
-
+            
             foreach (var file in files)
             {
                 try
                 {
-                    var outputPath = await EncodeVideo(file, logo, fontName, fontSize, bitRate);
+                    var (outputPath,subtitleTempPath) = await EncodeVideo(file, logo, fontName, fontSize, bitRate, enableCmd);
                     if (CheckAvailableSize(file, outputPath))
                     {
                         Console.WriteLine($"Encode thành công: {file}");
-                        File.Delete(file);
+                        if (deleteAfterEncode)
+                        {
+                            Console.WriteLine($"Xoá thành công {file}");
+                        }
                     }
                     else
                     {
@@ -65,10 +81,11 @@ namespace FFMPEGEncoder
                         Console.WriteLine($"Encode thất bại - {file}");
                         Console.ResetColor();
                     }
+                    if(File.Exists(subtitleTempPath)) File.Delete(subtitleTempPath);
                 }
                 catch (Exception e)
                 {
-                    
+
                     Console.WriteLine($"Lỗi {e}");
                 }
                 finally
@@ -81,43 +98,19 @@ namespace FFMPEGEncoder
             Console.ReadLine();
         }
 
-        static async Task<List<Episode>> GetMkvFiles()
+
+        private static async Task<(string,string)> EncodeVideo(string inputFile, string logo, string fontName, int fontSize, int bitRate,bool enableCmd)
         {
 
-            var conn = new SqlConnection("Data Source=TalonEzio;Initial Catalog=BiliBiliDownloader;Integrated Security=True;TrustServerCertificate=True");
+            var subtitlePath = await ExportSubtitle(inputFile, enableCmd);
 
-            return (await conn.QueryAsync<Episode>("Select * from Episodes where (Encoded is null or encoded = 0) and Done = 1")).ToList();
-        }
 
-        static async Task UpdateEncodePath(int episodeId, string encodePath, bool done)
-        {
-            var doneInt = done ? 1 : 0;
-
-            var conn = new SqlConnection("Data Source=TalonEzio;Initial Catalog=BiliBiliDownloader;Integrated Security=True;TrustServerCertificate=True");
-            await conn.OpenAsync();
-            var result = await conn.ExecuteReaderAsync("Update episodes set Done=@doneInt, EncodePath = @encodePath,Encoded = @doneInt,Path = NULL" +
-                                          " where Id = @episodeId",
-                new
-                {
-                    encodePath,
-                    episodeId,
-                    doneInt
-                });
-            await conn.CloseAsync();
-        }
-
-        private static async Task<string> EncodeVideo(string inputFile, string logo, string fontName, int fontSize, int bitRate)
-        {
-
-            var subtitlePath = inputFile.Contains("'") ? await ExportSubtitle(inputFile) : inputFile;
-
-            
             var outputFile = Path.ChangeExtension(inputFile, ".mp4");
 
             if (File.Exists(outputFile) && File.Exists(inputFile) && CheckAvailableSize(inputFile, outputFile))
             {
                 Console.WriteLine("File đã xử lý xong");
-                return outputFile;
+                return (outputFile,subtitlePath);
             }
 
             var argumentsBuilder = new StringBuilder();
@@ -126,19 +119,22 @@ namespace FFMPEGEncoder
             argumentsBuilder.Append("-filter_complex \"");
             argumentsBuilder.Append("[0:v][1:v] overlay=x=main_w-overlay_w-(main_w*0.01):y=main_h*0.01, ");
             argumentsBuilder.Append($"subtitles='{EscapeString(DoubleSlash(subtitlePath))}':force_style='Fontname={fontName},FontSize={fontSize}'\" ");
-            argumentsBuilder.Append($"-c:v h264_nvenc -b:v {bitRate}k -c:a copy ");
+            argumentsBuilder.Append($"-c:v h264_nvenc -b:v {bitRate}k -c:a aac ");
             argumentsBuilder.Append($"\"{outputFile}\"");
-            argumentsBuilder.Append(" -y");
-            
+            argumentsBuilder.Append(" -y"); //Force encoding
+
+
             string arguments = argumentsBuilder.ToString();
 
+            var ffmpegPath = Path.Combine(BasePath, "Tools", "ffmpeg.exe");
             var startInfo = new ProcessStartInfo
             {
-                FileName = @"Tools\ffmpeg",
+                FileName = ffmpegPath,
                 Arguments = arguments,
-                UseShellExecute = true,
-                CreateNoWindow = false
+                UseShellExecute = enableCmd,
+                CreateNoWindow = !enableCmd
             };
+
 
             using var process = new Process();
             process.StartInfo = startInfo;
@@ -147,29 +143,32 @@ namespace FFMPEGEncoder
             await process.WaitForExitAsync();
 
             File.Delete(subtitlePath);
-            return outputFile;
+            return (outputFile, subtitlePath);
         }
 
-        private static async Task<string> ExportSubtitle(string inputFile)
+        private static async Task<string> ExportSubtitle(string inputFile,bool enableCmd)
         {
             var argumentBuilder = new StringBuilder();
 
-            var randomFile = Path.GetRandomFileName();
-            randomFile = Path.ChangeExtension(randomFile, ".srt");
+            var tempPath = Path.GetTempPath();
+            var randomFile = Path.ChangeExtension(Path.GetRandomFileName(), ".srt");
 
+            randomFile = Path.Combine(tempPath, randomFile);
             argumentBuilder.Append($"tracks \"{inputFile}\" ");
             argumentBuilder.Append($"2:\"{randomFile}\"");
 
 
             var arguments = argumentBuilder.ToString();
 
+            var mkvExtractPath = Path.Combine(BasePath, "Tools", "mkvextract.exe");
             var startInfo = new ProcessStartInfo
             {
-                FileName = @"Tools\mkvextract",
+                FileName = mkvExtractPath,
                 Arguments = arguments,
-                UseShellExecute = false,
-                CreateNoWindow = true
+                UseShellExecute = enableCmd,
+                CreateNoWindow = !enableCmd
             };
+
 
             using var process = new Process();
             process.StartInfo = startInfo;
@@ -184,13 +183,12 @@ namespace FFMPEGEncoder
             var inputInfo = new FileInfo(inputFile);
             var outputInfo = new FileInfo(outputFile);
             return inputInfo.Length / 3 < outputInfo.Length;
-
         }
 
         private static string EscapeString(string input)
         {
             return input
-                //.Replace(":", @"\:")
+                .Replace(":", @"\:")
                 .Replace("'", @"''");
         }
 
